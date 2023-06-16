c7f17317fe6b028718f8dcce41b269ee65c55cc8
supreeta-anand-byatnal <supreeta.byatnal@gmail.com>
2023-06-16 23:35:59 +0530

    Navigation bar

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+import bg from '../src/img/bg.png';
+import {MainLayout} from './styles/Layouts'
+import React, { useState } from 'react'
+import Orb from './Components/Orb/Orb'
+import Navigation from './Components/Navigation/Navigation';
+
+
+function App() {
+
+  const [active, setActive] = useState(1)
+  return (
+
+    <AppStyled bg= {bg} className="App">
+
+      <Orb />
+      <MainLayout>
+
+          <Navigation active={active} setActive={setActive} />
+      </MainLayout>
+    </AppStyled>
+  );
+}
+
+const AppStyled = styled.div`
+height: 100vh;
+background-image: url(${props => props.bg});
+position: relative;
+`;
+export default App;
